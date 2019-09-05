c6cb6666be45fbce7ba647c724ce51e14bd3d8e3
Faith Mofokeng <faithmofokeng5@gmail.com>
2019-09-05 11:15:11 +0200

    Made one Add and Multiply function

--- spec/test_function.js
+++ spec/test_function.js
@@ -1,12 +1,12 @@
 describe("Add", function () {
 
     it("should add zeros", function () {
-        let answer1 = addZero(0,0);
+        let answer1 = add(0,0);
         expect(answer1).toBe(0);
     });
 
     it("should add negative numbers", function () {
-        let answer2 = addNeg(-1,-1);
+        let answer2 = add(-1,-1);
         expect(answer2).toBe(-2);
     });
 
@@ -16,7 +16,7 @@ describe("Add", function () {
     });
 
     it("should add multiple numbers", function () {
-        let answer4 = addMultiple(1,2,3,4);
+        let answer4 = add(1,2,3,4);
         expect(answer4).toBe(10);
     });
 
@@ -24,11 +24,11 @@ describe("Add", function () {
 
 describe("Multiply", function(){
     it("should multiply numbers", function () {
-        let answer5 = mutliply(1,2);
+        let answer5 = multiply(1,2);
         expect(answer5).toBe(2);
     });
     it("should multiply multiple numbers", function(){
-        let answer6= multiplyMul(1,2,3,4);
+        let answer6= multiply(1,2,3,4);
         expect(answer6).toBe(24);
     })
 
--- src/calculator.js
+++ src/calculator.js
@@ -1,24 +1,12 @@
-
-function addZero(g,h){
-        return g + h;
-    }
-function addNeg(c,d){
-        return c + d;
-    };
 function add(a,b){
-        return a + b;
-    };
-function addMultiple(){
-        var sum=0;
+    var sum=0;
         for(let x=0; x<arguments.length; x++){
-            sum +=arguments[x];
-        }
-        return sum;
-    };
-function mutliply(i,j){
-        return i * j;
-    };
-function multiplyMul(){
+        sum +=arguments[x];
+    }
+    return sum;
+}
+
+function multiply(){
         var product=1;
         for(let x=0; x<arguments.length; x++){
             product *= arguments[x];
@@ -26,6 +14,7 @@ function multiplyMul(){
         return product;
     };
 
+multiply(3,4);
 
 
 
